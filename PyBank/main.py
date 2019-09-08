e54b8e9a8532399c1214b0d41a2f49c0f8d75057
Jiao <ruyi.jiao@gmail.com>
2019-09-08 14:37:33 -0400

    initial commit

---
+++ PyBank/main.py
@@ -0,0 +1,62 @@
+# Import the pathlib and csv library 
+
+
+# set the file path 
+
+# initialize total month variable 
+
+# initialize variable to hold the monthly pnl list 
+
+# open the input path as a file object 
+
+    # pass in the csv file to the csv.reader() function 
+    
+    # print the datatype of the csvreader 
+    
+    # read the header
+    
+    # print the header 
+    
+    # read each row of data after the header with for loop
+    
+        # print the row 
+        
+        # set monthly pnl variable to the second column of each row, change the data type fo integer 
+        
+        # append the rwo monthly pnl value to the list of monthly pnl
+
+#initialize metric variables 
+
+min_mpnl = 0
+max_mpnl = 0 
+avg_change = 0 
+total_change = 0 
+count_month = 0
+
+# calculate the max, min, mean of the list of monthly pnl with for loop 
+
+    # sum the total and count variables 
+    
+    
+    # logic to determin min and max 
+    
+# calculate the average and round 
+    
+# print the metrics 
+
+# set the output file path 
+
+# open the output path as a file object
+
+    # set the file object as a csvwriter object
+    
+    # write the header to the output file 
+    
+    # write the list of metrics to the output file 
+
+    
+    
+    
+    
+    
+
